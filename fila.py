d215c87415afcab0f331b1192fb5d0e052742dff
Yan <141442732+YanMancini@users.noreply.github.com>
2023-09-18 16:59:57 -0300

    Create fila.py

---
+++ fila.py
@@ -0,0 +1,14 @@
+import filafunções as f
+import os
+import random as r
+fila=[]
+numbre=[]
+op=1
+op2=0
+posi=-1
+oq=0
+f.adicionarpaciente(op)
+while op2!=1:
+    posi=posi+1
+    f.mostrarlista()
+    
